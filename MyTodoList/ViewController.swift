64931426f6ca8afd5610007b5d48ee94425a830f
katorie <katoriexxxkatorie@gmail.com>
2018-04-20 16:15:17 +0900

    入力していく動作まで実装

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by 加藤理絵 on 2018/04/20.
+//  Copyright © 2018年 Rie Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var todoList = [String]()
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func tapAddButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Todo追加", message: "Todoを入力してください", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addTextField(configurationHandler: nil)
+
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action: UIAlertAction) in
+            if let textField = alertController.textFields?.first {
+                self.todoList.insert(textField.text!, at: 0)
+                self.tableView.insertRows(at: [IndexPath(row: 0, section:0)], with: UITableViewRowAnimation.right)
+            }
+        }
+
+        alertController.addAction(okAction)
+
+        let cancelButton = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+        alertController.addAction(cancelButton)
+
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        let todoTitle = todoList[indexPath.row]
+        cell.textLabel?.text = todoTitle
+        
+        return cell
+    }
+}
+
