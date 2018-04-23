fb7e4bf98e5e545d76e2e7f3a6fe8fea9787346d
katorie <katoriexxxkatorie@gmail.com>
2018-04-23 11:14:51 +0900

    チェックマークをつけられるようにする

--- MyTodoList/ViewController.swift
+++ MyTodoList/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    var todoList = [String]()
+    var todoList = [MyTodo]()
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -19,11 +19,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
         let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action: UIAlertAction) in
             if let textField = alertController.textFields?.first {
-                self.todoList.insert(textField.text!, at: 0)
+                let myTodo = MyTodo()
+                myTodo.todoTitle = textField.text!
+                self.todoList.insert(myTodo, at: 0)
+                
                 self.tableView.insertRows(at: [IndexPath(row: 0, section:0)], with: UITableViewRowAnimation.right)
                 
                 let userDefaults = UserDefaults.standard
-                userDefaults.set(self.todoList, forKey: "todoList")
+                let data = NSKeyedArchiver.archivedData(withRootObject: self.todoList)
+                userDefaults.set(data, forKey: "todoList")
                 userDefaults.synchronize()
             }
         }
@@ -40,8 +44,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         
         let userDefaults = UserDefaults.standard
-        if let storedTodoList = userDefaults.array(forKey: "todoList") as? [String] {
-            todoList.append(contentsOf: storedTodoList)
+        if let storedTodoList = userDefaults.object(forKey: "todoList") as? Data {
+            if let unarchiveTodoList = NSKeyedUnarchiver.unarchiveObject(with: storedTodoList) as? [MyTodo] {
+                todoList.append(contentsOf: unarchiveTodoList)
+            }
         }
     }
 
@@ -56,10 +62,52 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
-        let todoTitle = todoList[indexPath.row]
-        cell.textLabel?.text = todoTitle
+        let myTodo = todoList[indexPath.row]
+        cell.textLabel?.text = myTodo.todoTitle
+        
+        if myTodo.todoDone {
+            cell.accessoryType = UITableViewCellAccessoryType.checkmark
+        } else {
+            cell.accessoryType = UITableViewCellAccessoryType.none
+        }
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let myTodo = todoList[indexPath.row]
+        
+        if myTodo.todoDone {
+            myTodo.todoDone = false
+        } else {
+            myTodo.todoDone = true
+        }
+        
+        tableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+        
+        let data: Data = NSKeyedArchiver.archivedData(withRootObject: todoList)
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(data, forKey: "todoList")
+        userDefaults.synchronize()
+    }
 }
 
+class MyTodo: NSObject, NSCoding {
+    var todoTitle: String?
+    var todoDone: Bool = false
+    
+    // コンストラクタ？
+    override init() {
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        todoTitle = aDecoder.decodeObject(forKey: "todoTitle") as? String
+        // todoDone = aDecoder.decodeObject(forKey: "todoDone") -> Optional type 'Any?' cannot be used as a boolean; test for '!= nil' instead
+        todoDone = (aDecoder.decodeObject(forKey: "todoDone") != nil)
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(todoTitle, forKey: "todoTitle")
+        aCoder.encode(todoDone, forKey: "todoDone")
+    }
+}
